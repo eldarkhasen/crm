eabaf557f456240ff98363dc251d379bb498704d
eldarkhasen <eldarkhasen@gmail.com>
2019-02-10 14:27:17 +0600

    Это тестовый коммит и пуш. Создал новую ветку для пациентов и создал для них контроллер, модель и миграцию.

---
+++ app/Http/Controllers/PatientsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Patients;
+use Illuminate\Http\Request;
+
+class PatientsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Patients  $patients
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Patients $patients)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Patients  $patients
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Patients $patients)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Patients  $patients
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Patients $patients)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Patients  $patients
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Patients $patients)
+    {
+        //
+    }
+}
